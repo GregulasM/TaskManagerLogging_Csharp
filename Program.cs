7037942d1229ffda8ffd253c305cbca4ca2b52da
GregulasM <miha125rus@yandex.ru>
2025-04-07 05:39:56 +1000

    Добавлено отслеживание событий, методов, содержания переменных, добавлено логирование в json

--- Program.cs
+++ Program.cs
@@ -34,17 +34,14 @@ namespace TaskManager
                 .ReadFrom.Configuration(configuration) 
                 .CreateLogger();
             
-            Log.Information("Приложение успешно запущено (через appsettings.json)!");
-
-            // Задаем вывод в консоль и в текстовый файл, делаем автозапись в файл.
-            Trace.Listeners.Add(new ConsoleTraceListener());
-            Trace.Listeners.Add(new TextWriterTraceListener("TaskManager.txt"));
-            Trace.AutoFlush = true;
-            Trace.TraceInformation("[INFO] Программа TaskManager запущена.");
+            Log.Information("------Приложение успешно запущено (через appsettings.json)------");
+            
+            Log.Information("Запуск цикла программы");
 
             // Крутим приложение в цикле и даем на выбор несколько вариантов взаимодействия. При выходе завершаем работу программы. Вводим цифры, 1, 2, 3, 4 для работы.
             while (true)
             {
+                Log.Information("Выбор вариантов ответа и ввод данных");
                 Console.WriteLine("Выберите вариант команды:");
                 Console.WriteLine("1. Добавить задачу. \n2. Удалить задачу. \n3. Все задачи. \n4. Выход из программы.");
                 string выбор = Console.ReadLine();
@@ -53,28 +50,31 @@ namespace TaskManager
                 switch (выбор)
                 {
                     case "1":
+                        Log.Information("Производится добавление задачи");
                         Console.WriteLine("Введите название задачи:");
                         string название_задачи = Console.ReadLine();
                         AddTask(название_задачи);
                         break;
 
                     case "2":
+                        Log.Information("Производится удаление задачи");
                         Console.WriteLine("Введите название задачи для удаления:");
                         string removeTitle = Console.ReadLine();
                         RemoveTask(removeTitle);
                         break;
 
                     case "3":
+                        Log.Information("Производится вывод списка задач");
                         ListTasks();
                         break;
                     
                     case "4":
-                        Trace.TraceInformation("[INFO] Завершение работы программы.");
-                        Trace.WriteLine("---------------------------------------------");
+                        Log.Information("------Завершение работы программы------");
                         Environment.Exit(0);
                         break;
 
                     default:
+                        Log.Information("Правильного ввода команды не произошло");
                         Console.WriteLine("Неверная команда. Пробуй еще раз.");
                         break;
                 }
@@ -84,55 +84,59 @@ namespace TaskManager
         // Добавление задачи в список. Проверка на дубликаты отсутствует. Имеется проверка IsNullOrWhiteSpace.
         private static void AddTask(string название_задачи)
         {
-            Trace.WriteLine("[TRACE] Начало операции AddTask.");
+            Log.Information("Начало операции AddTask()");
             if (string.IsNullOrWhiteSpace(название_задачи))
             {
-                Trace.TraceWarning("[WARN] Попытка добавить задачу с пустым названием. Операция не выполнена.");
-                Trace.WriteLine("[TRACE] Конец операции AddTask.");
+                Log.Warning("Попытка добавить задачу с пустым названием. Операция не выполнена");
+                Log.Information("Конец добавления задачи AddTask()");
                 return;
             }
 
             Task newTask = new Task(название_задачи);
             tasks.Add(newTask);
-            Trace.TraceInformation($"[INFO] Задача «{название_задачи}» успешно добавлена.");
-            Trace.WriteLine("[TRACE] Конец операции AddTask.");
+            Console.WriteLine($"Задача «{название_задачи}» добавлена.");
+            
+            Log.Information($"Задача «{название_задачи}» успешно добавлена");
+            Log.Information("Конец операции AddTask()");
         }
 
         // Удаление задачи из списка благодаря LinQ- производим поиск, удаляем совпадения. Если не нашли, то держим в курсе, что такой задачи не нашли.
         private static void RemoveTask(string название_задачи)
         {
-            Trace.WriteLine("[TRACE] Начало операции RemoveTask.");
+            Log.Information("Начало операции RemoveTask()");
             Task taskToRemove = tasks.Find(t => t.Title == название_задачи);
             if (taskToRemove == null)
             {
-                Trace.TraceError($"[ERROR] Задача «{название_задачи}» не найдена.");
-                Trace.WriteLine("[TRACE] Конец операции RemoveTask.");
+                Log.Error($"Задача «{название_задачи}» не найдена.");
+                Log.Information("Конец операции RemoveTask()");
                 return;
             }
 
             tasks.Remove(taskToRemove);
-            Trace.TraceInformation($"[INFO] Задача «{название_задачи}» успешно удалена.");
-            Trace.WriteLine("[TRACE] Конец операции RemoveTask.");
+            Log.Information($"Задача «{название_задачи}» операции RemoveTask() успешно удалена.");
+            Log.Information("Конец операции RemoveTask()");
         }
 
         // Проверка списка на наличие задач - если есть, то выводим их в цикле, если нет, то говорим, что пусто.
         private static void ListTasks()
         {
-            Trace.WriteLine("[TRACE] Начало операции ListTasks.");
+            Log.Information("Начало операции ListTasks()");
             if (tasks.Count == 0)
             {
-                Trace.TraceInformation("[INFO] Список задач пуст.");
+                Log.Information("Выведенный список задач пуст");
                 Console.WriteLine("Список задач пуст.");
             }
             else
             {
-                Trace.TraceInformation($"[INFO] Всего задач: {tasks.Count}.");
+                Log.Information($"Всего выведено задач: {tasks.Count}");
                 for (int i = 0; i < tasks.Count; i++)
                 {
                     Console.WriteLine($"{i + 1}. {tasks[i].Title}");
                 }
+
+                Log.Information("{@Tasks}", tasks);
             }
-            Trace.WriteLine("[TRACE] Конец операции ListTasks.");
+            Log.Information("Конец операции ListTasks()");
         }
     }
 }
